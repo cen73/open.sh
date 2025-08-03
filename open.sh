13331bd5397f43eb7b1f00df4415eb173316426c
cen73 <chencq60@gmail.com>
2025-08-03 14:18:53 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://github.com/Yxiguan/OpenList_123/releases/download/v4.0.9/openlist-linux-arm-7.tar.gz https://cen73.github.io/open.sh/openlist.service
 tar -zxvf openlist-linux-arm-7.tar.gz
-mv openlist.service /usr/lib/systemd/system/
+rm openlist-linux-arm-7.tar.gz
 ./openlist server
-
+mv openlist.service /usr/lib/systemd/system/
