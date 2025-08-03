d93f2f7178ea63d834e2d94d430d18f870ecd976
cen73 <chencq60@gmail.com>
2025-08-03 15:08:04 +0800

    Update openlist.sh

--- openlist.sh
+++ openlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/Yxiguan/OpenList_123/releases/download/v4.0.9/openlist-linux-arm-7.tar.gz https://cen73.github.io/open.sh/openlist.service https://cen73.github.io/open.sh/run.sh
+wget https://github.com/Yxiguan/OpenList_123/releases/download/v4.0.9/openlist-linux-arm-7.tar.gz https://cen73.github.io/open.sh/openlist.service 
 
 tar -zxvf openlist-linux-arm-7.tar.gz
 
@@ -8,8 +8,14 @@ rm openlist-linux-arm-7.tar.gz
 
 mv -f openlist.service /usr/lib/systemd/system/
 
-chmod +x run.sh
+echo "重载配置"
+systemctl daemon-reload
+echo "运行"
+systemctl start openlist
+echo "配置开机自启"
+systemctl enable openlist
 
+echo "OpenList 安装完成并已启动"
 
 
 
